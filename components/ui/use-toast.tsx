3aa974871e56d03330299692f60f1efbd4865c12
fahryafc <fahry.afc@gmail.com>
2024-11-23 17:31:00 +0700

    fixing bugs

--- components/ui/use-toast.tsx
+++ components/ui/use-toast.tsx
@@ -26,7 +26,12 @@ function genId() {
   return count.toString();
 }
 
-type ActionType = typeof actionTypes;
+type ActionType = {
+  ADD_TOAST: "ADD_TOAST";
+  UPDATE_TOAST: "UPDATE_TOAST";
+  DISMISS_TOAST: "DISMISS_TOAST";
+  REMOVE_TOAST: "REMOVE_TOAST";
+};
 
 type Action =
   | {
